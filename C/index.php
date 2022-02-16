13860c8e825c7e62a48f582160906aa2fe791576
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-16 10:14:10 +0100

    Sneak C completato

--- C/index.php
+++ C/index.php
@@ -6,6 +6,24 @@
     <title>Snack C</title>
 </head>
 <body>
-    
+    <div style="text-align: center">
+        <?php
+        $nMax = 100;
+        $nMin = 0;
+
+        $numbersArray = [];
+        
+        while (count($numbersArray) < 15) {
+            $randomNumber = mt_rand($nMin, $nMax);
+
+            if (!in_array($randomNumber, $numbersArray)) {
+                $numbersArray[] = $randomNumber;
+            }
+        }
+        
+        echo '<pre>' . var_dump($numbersArray) . '</pre>';
+        ?>
+    </div>
 </body>
+
 </html>
