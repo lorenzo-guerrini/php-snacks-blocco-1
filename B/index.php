5e0113d5b9fecfbb42445abae28449e761a8931a
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-16 09:35:39 +0100

    Snack B iniziato

--- B/index.php
+++ B/index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack B</title>
 </head>
+
 <body>
-    
+    <?php
+    $name = $_GET["name"];
+    $mail = $_GET["mail"];
+    $age = $_GET["age"];
+
+    echo $name;
+    echo $age;
+    echo $mail;
+    ?>
 </body>
 </html>
