7d6c5ff425f07822421f44af5606c40000771545
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-16 10:20:34 +0100

    Snack D finito

--- D/index.php
+++ D/index.php
@@ -5,7 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack D</title>
 </head>
+
 <body>
-    
+    <div style="text-align: center">
+
+        <?php
+            $paragraph = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ipsa quam culpa magnam eligendi ullam dolorum suscipit nulla perferendis enim neque, iusto perspiciatis eius, velit iure ipsum assumenda, in cupiditate sequi consequatur modi! Molestias, ipsa. Delectus perspiciatis minus incidunt exercitationem necessitatibus consectetur officiis vero, eum ratione aspernatur magni sapiente temporibus sint possimus quis sunt facere laudantium suscipit beatae fuga voluptas saepe ad aliquid hic. Similique error laudantium explicabo molestiae modi dolor consequuntur illum eveniet reiciendis voluptas unde, earum voluptatibus magnam quidem. Sit pariatur nostrum temporibus, omnis, ab nobis placeat aspernatur aliquid, suscipit minus consectetur voluptatem eius nesciunt possimus velit doloremque maxime iusto ut? Enim maiores soluta, repudiandae consequatur a aliquam alias quae nesciunt molestiae voluptatibus commodi tenetur et sit necessitatibus quod natus numquam quasi suscipit deserunt ducimus. Fugit, natus quisquam voluptates iure corporis cumque repellendus, odit magni perferendis, et doloremque quis dicta dolor sunt labore ut nostrum. Dolor, nihil, consequuntur ipsa totam voluptatibus illo mollitia laboriosam reiciendis qui repudiandae fugit fugiat in eius fuga quisquam recusandae, iure placeat adipisci odio quibusdam impedit aperiam necessitatibus nisi distinctio? Adipisci nisi recusandae magni temporibus cupiditate dolores odio laudantium, soluta in earum iste enim deleniti quibusdam. Vitae, consequatur explicabo? Distinctio voluptates incidunt architecto ea';
+
+            $paragraphArray = explode('. ', $paragraph);
+
+            echo '<pre>' . var_dump($paragraphArray) . '</pre>';
+        ?>
+
+    </div>    
 </body>
 </html>
